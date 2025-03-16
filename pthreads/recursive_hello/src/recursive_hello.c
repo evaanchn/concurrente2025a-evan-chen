5dc11e9f5f3dd02fd991a309e12e3e8c15811f36
Evan <evan.chen@ucr.ac.cr>
2025-03-16 00:53:31 -0600

    Recursive hello completed

---
+++ pthreads/recursive_hello/src/recursive_hello.c
@@ -0,0 +1,60 @@
+// Copyright 2021 Jeisson Hidalgo <jeisson.hidalgo@ucr.ac.cr> CC-BY 4.0
+// Edited by Evan Chen
+#include <pthread.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * @brief Recursive procedure that bids farewell and reports number if parameter received is 0, 
+ * otherwise it greets and reports the number, creating another thread to call itself with 1 num less than parameter.
+ * @param void* data. Void pointer that will become a size_t* to use inside procedure.
+ * @return NULL
+ */
+void* greet(void* data);
+
+/**
+ * @brief Main procedure of program. Creates secondary thread to greet from recursively and greet itself.
+ * @param void. No parameters.
+ * @return EXIT_SUCCESS (0) to the O.S if nothing went wrong. EXIT_FAILURE (1) if there was an error.
+ */
+// procedure main:
+int main(void) {
+  size_t num = 2; // Local variable initialized in 2
+
+  //pthread creation
+  pthread_t thread;
+  int error = pthread_create(&thread, /*attr*/ NULL, greet, &num);
+  if (error == EXIT_SUCCESS) {
+    // print "Hello from main thread"
+    printf("Hello from main thread\n");
+
+    // Wait for created thread to finish executing (which could also be waiting)
+    pthread_join(thread, /*value_ptr*/ NULL);
+  } else {
+    fprintf(stderr, "Error: could not create secondary thread\n");
+  }
+  return error;
+}  // end procedure
+
+// procedure greet:
+void* greet(void* data) {
+  size_t* number = (size_t*) data; // Casting pointer to original type.
+  // Bid farewell if number is 0
+  if (*number == 0){
+    printf("Goodbye from secondary thread! Received number: %zu\n", *number);
+  }
+  // Greet and report number otherwise
+  else{
+    printf("Hello from secondary thread! Received number: %zu\n", (*number)--);
+    
+    pthread_t thread;
+    int error = pthread_create(&thread, /*attr*/ NULL, greet, data);
+    if (error != EXIT_SUCCESS){
+      fprintf(stderr, "Error: could not create secondary thread\n");
+    }
+    else{
+      pthread_join(thread, /*value_ptr*/ NULL);
+    } 
+  }
+  return NULL;
+}  // end procedure
