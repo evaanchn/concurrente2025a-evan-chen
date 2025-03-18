4cdf867a7ae584a6b2e349e0560fe27d1b67fed2
Evan <evan.chen@ucr.ac.cr>
2025-03-18 11:35:43 -0600

    invert: internal documentation for main(). Closes #1

--- pthreads/invert/src/solution.c
+++ pthreads/invert/src/solution.c
@@ -10,15 +10,26 @@
  * @return Status code to the operating system, 0 means success.
  */
 int main(void) {
-  long numbers[1000];
-  size_t count = 0;
-  while (scanf("%ld", &numbers[count]) ==  1){
-    ++count;
+  // Create a dynamic array to store the numbers, given we don't know the number of elements beforehand
+  size_t count = 0, capacity = 1000;
+  long* numbers = malloc(capacity * sizeof(long));
+
+  // Read numbers until end-of-file
+  while (scanf("%ld", &numbers[count]) ==  1) {
+    // If capacity is reached
+    if (++count == capacity) {
+      // expand capacity of array ten-fold
+      capacity *= 10;
+      numbers = realloc(numbers, capacity * sizeof(long));
+    }
   }
 
-  for (long index = count - 1; index >= 0; --index){
+  // Print all numbers in inverse order
+  for (long index = count - 1; index >= 0; --index) {
     printf("%ld\n", numbers[index]);
   }
 
+  // Deallocate dynamic array
+  free(numbers);
   return EXIT_SUCCESS;
 }
