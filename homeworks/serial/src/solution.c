8c387537333a5a9d86084e05ddde7ce3af12dcd7
Evan <evan.chen@ucr.ac.cr>
2025-03-23 00:35:31 -0600

    Started Problem Statement for Analysis in homeworks/serial

---
+++ homeworks/serial/src/solution.c
@@ -0,0 +1,18 @@
+// Copyright <year> <You>
+#include <assert.h>
+#include <stdbool.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * @brief Start program execution.
+ *
+ * @return Status code to the operating system, 0 means success.
+ */
+int main(void) {
+  // TODO(you): Analyze the problem and be sure you understand each piece of
+  // input and output. Solve the problem by creating an algorithm, and test it.
+  // Copy the algorithm within source code comments. Finally implement the
+  // algorithm's instructions in the C applying good programming practices.
+  return EXIT_SUCCESS;
+}
