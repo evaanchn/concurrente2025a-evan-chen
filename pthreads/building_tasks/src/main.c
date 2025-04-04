0705ea3665bf8de61090eaee783e372f7ad88f76
Evan <evan.chen@ucr.ac.cr>
2025-04-04 11:39:42 -0600

    pthreads/building_tasks: Completed building_tasks

--- pthreads/building_tasks/src/main.c
+++ pthreads/building_tasks/src/main.c
@@ -2,15 +2,26 @@
 #define _XOPEN_SOURCE 500
 
 #include <assert.h>
+#include <semaphore.h>  // Used to be files
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
 #include <unistd.h>
 
-#include "ethread.h"
+#include "ethread.h"  // ECCI-Threads Wrapper
 
 struct shared_data {
   useconds_t max_duration;  // microseconds
+  // int sem_init(sem_t *sem, int pshared, unsigned int value);
+  // pshared = process shared
+  sem_t walls_ready;
+  sem_t exterior_plumbing_ready;
+  sem_t roof_ready;
+  sem_t exterior_painting_ready;
+  sem_t interior_plumbing_ready;
+  sem_t electrical_installation_ready;
+  sem_t interior_painting_ready;
+  sem_t floor_ready;
 };
 
 int do_concurrent_tasks(struct shared_data* shared);
@@ -33,7 +44,29 @@ int main(int argc, char* argv[]) {
   if (argc == 2 && sscanf(argv[1], "%u", &shared->max_duration) == 1 &&
       shared->max_duration) {
     srand48(time(NULL) + clock());
+
+    sem_init(&shared->walls_ready, 0, 0);
+    sem_init(&shared->exterior_plumbing_ready, 0, 0);
+    sem_init(&shared->roof_ready, 0, 0);
+    sem_init(&shared->exterior_painting_ready, 0, 0);
+    sem_init(&shared->interior_plumbing_ready, 0, 0);
+    sem_init(&shared->electrical_installation_ready, 0, 0);
+    sem_init(&shared->interior_painting_ready, 0, 0);
+    sem_init(&shared->floor_ready, 0, 0);
+
     do_concurrent_tasks(shared);
+
+    // Operating system protects semaphores and mutex with
+    // atomic instructions (only one core can execute at a time, meaning only
+    // one thread could decrement semaphore at once, protected by hardware)
+    sem_destroy(&shared->walls_ready);
+    sem_destroy(&shared->exterior_plumbing_ready);
+    sem_destroy(&shared->roof_ready);
+    sem_destroy(&shared->exterior_painting_ready);
+    sem_destroy(&shared->interior_plumbing_ready);
+    sem_destroy(&shared->electrical_installation_ready);
+    sem_destroy(&shared->interior_painting_ready);
+    sem_destroy(&shared->floor_ready);
   } else {
     fprintf(stderr, "Usage: building_tasks max_microseconds_duration\n");
     error = EXIT_FAILURE;
@@ -43,7 +76,7 @@ int main(int argc, char* argv[]) {
 }
 
 int do_concurrent_tasks(struct shared_data* shared) {
-  struct thread_team* team = reserve_threads(11, shared);
+  struct thread_team* team = reserve_threads(10, shared);
   assert(team);
   add_thread(team, walls);
   add_thread(team, exterior_plumbing);
@@ -65,51 +98,65 @@ void* walls(void* data) {
   assert(shared);
   usleep(lrand48() % shared->max_duration);
   puts("1.2 walls finish");
+  sem_post(&shared->walls_ready);
+  sem_post(&shared->walls_ready);
   return NULL;
 }
 
 void* exterior_plumbing(void* data) {
   struct shared_data* shared = (struct shared_data*) get_shared_data(data);
+  sem_wait(&shared->walls_ready);
   puts("2.1 exterior plumbing start");
   usleep(lrand48() % shared->max_duration);
   puts("2.1 exterior plumbing finish");
+  sem_post(&shared->exterior_plumbing_ready);
+  sem_post(&shared->exterior_plumbing_ready);
   return NULL;
 }
 
 void* roof(void* data) {
   struct shared_data* shared = (struct shared_data*) get_shared_data(data);
+  sem_wait(&shared->walls_ready);
   puts("2.3 roof start");
   usleep(lrand48() % shared->max_duration);
   puts("2.3 roof finish");
+  sem_post(&shared->roof_ready);
   return NULL;
 }
 
 void* exterior_painting(void* data) {
   struct shared_data* shared = (struct shared_data*) get_shared_data(data);
+  sem_wait(&shared->exterior_plumbing_ready);
   puts("3.1 exterior painting start");
   usleep(lrand48() % shared->max_duration);
   puts("3.1 exterior painting finish");
+  sem_post(&shared->exterior_painting_ready);
   return NULL;
 }
 
 void* interior_plumbing(void* data) {
   struct shared_data* shared = (struct shared_data*) get_shared_data(data);
+  sem_wait(&shared->exterior_plumbing_ready);
   puts("3.2 interior plumbing start");
   usleep(lrand48() % shared->max_duration);
   puts("3.2 interior plumbing finish");
+  sem_post(&shared->interior_plumbing_ready);
   return NULL;
 }
 
 void* electrical_installation(void* data) {
   struct shared_data* shared = (struct shared_data*) get_shared_data(data);
+  sem_wait(&shared->roof_ready);
   puts("3.3 electrical installation start");
   usleep(lrand48() % shared->max_duration);
   puts("3.3 electrical installation finish");
+  sem_post(&shared->electrical_installation_ready);
   return NULL;
 }
 
 void* exterior_finishes(void* data) {
   struct shared_data* shared = (struct shared_data*) get_shared_data(data);
+  sem_wait(&shared->exterior_painting_ready);
   puts("4.1 exterior finishes start");
   usleep(lrand48() % shared->max_duration);
   puts("4.1 exterior finishes finish");
@@ -118,22 +165,28 @@ void* exterior_finishes(void* data) {
 
 void* interior_painting(void* data) {
   struct shared_data* shared = (struct shared_data*) get_shared_data(data);
+  sem_wait(&shared->interior_plumbing_ready);
+  sem_wait(&shared->electrical_installation_ready);
   puts("4.3 interior painting start");
   usleep(lrand48() % shared->max_duration);
   puts("4.3 interior painting finish");
+  sem_post(&shared->interior_painting_ready);
   return NULL;
 }
 
 void* floors(void* data) {
   struct shared_data* shared = (struct shared_data*) get_shared_data(data);
+  sem_wait(&shared->interior_painting_ready);
   puts("5.3 floor start");
   usleep(lrand48() % shared->max_duration);
   puts("5.3 floor finish");
+  sem_post(&shared->floor_ready);
   return NULL;
 }
 
 void* interior_finishes(void* data) {
   struct shared_data* shared = (struct shared_data*) get_shared_data(data);
+  sem_wait(&shared->floor_ready);
   puts("6.3 interior finishes start");
   usleep(lrand48() % shared->max_duration);
   puts("6.3 interior finishes finish");
