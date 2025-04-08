f4296013fbfba8d135c2596f110ae8d034e9ea15
Evan <evan.chen@ucr.ac.cr>
2025-04-08 09:21:12 -0600

    homeworks/pthreads: Adapted argument analyzing function from pthreads/prod_cons_bound and updated errors

--- homeworks/pthread/src/main.c
+++ homeworks/pthread/src/main.c
@@ -8,6 +8,8 @@
 
 #include "job.h"
 
+int analyze_arguments(int argc, char* argv[], uint64_t* thread_count);
+
 /**
  * @brief Processes execution command to set thread count and 
  *        manage if job file was specified. Calls simulate.
@@ -19,18 +21,29 @@ int main(int argc, char* argv[]) {
   // Amount of threads to use in simulation (IMPLEMENTED FOR HW2)
   uint64_t thread_count = 1;
 
-  if (argc < 2) {
-    // print "Error: No job file specified"
-    perror("ERROR: No job file specified\n");
-    return NO_JOB_FILE_SPECIFIED;
-  }  // else if (argc == 2) {
-  //   // Taken from hello_w example, calls sysconf to get available cores.
-  //   thread_count = sysconf(_SC_NPROCESSORS_ONLN);
-  // } else {
-  //   // Amount of threads to use specified in command
-  //   sscanf(argv[2], "%" SCNu64, &thread_count);
-  // }
+  int error = analyze_arguments(argc, argv, &thread_count);
 
-  int error = simulate(argv[1], thread_count);
+  if (error == EXIT_SUCCESS) error = simulate(argv[1], thread_count);
+
+  return error;
+}
+
+int analyze_arguments(int argc, char* argv[], uint64_t* thread_count) {
+  int error = EXIT_SUCCESS;
+  // Must at least include job directory
+  if (argc == 3) {
+    if (sscanf(argv[2], "%zu", thread_count) != 1
+        || *thread_count <= 0 || *thread_count > 32000) {
+      // Inform usage to user
+      fprintf(stderr,
+        "Error: Invalid thread count (0 < thread_count <= 32000)\n");
+      error = ERR_INVALID_THREAD_COUNT;
+    }
+  } else if (argc < 2) {
+    // Inform usage to user
+    fprintf(stderr,
+        "usage: bin/pthread job_file_path thread_count (count optional)\n");
+    error = ERR_NO_JOB_FILE;
+  }
   return error;
 }
