f4373027a6482014cdcca518ffa4e5420a1d99a5
Evan <evan.chen@ucr.ac.cr>
2025-05-07 10:53:11 -0600

    homeworks/pthread: Added flattened matrix to concurrent solution

--- homeworks/pthread/src/plate_matrix.c
+++ homeworks/pthread/src/plate_matrix.c
@@ -16,14 +16,14 @@ plate_matrix_t* init_plate_matrix(uint64_t rows, uint64_t cols) {
   plate_matrix->cols = cols;
 
   // Allocate the main matrix
-  plate_matrix->matrix = create_double_matrix(rows, cols);
+  plate_matrix->matrix = (double*) calloc(rows*cols, sizeof(double));
   if (!plate_matrix->matrix) {
     free(plate_matrix);
     return NULL;
   }
 
   // Allocate the auxiliary matrix
-  plate_matrix->auxiliary_matrix = create_double_matrix(rows, cols);
+  plate_matrix->auxiliary_matrix = (double*) calloc(rows*cols, sizeof(double));
   if (!plate_matrix->auxiliary_matrix) {
     free(plate_matrix->matrix);  // Free main matrix before returning
     free(plate_matrix);
@@ -36,30 +36,32 @@ plate_matrix_t* init_plate_matrix(uint64_t rows, uint64_t cols) {
 
 
 void init_auxiliary(plate_matrix_t* plate_matrix) {
-  double** initial_matrix = plate_matrix->matrix;
+  double* initial_matrix = plate_matrix->matrix;
 
   // Copy first row to auxiliary matrix
   for (uint64_t col = 0; col < plate_matrix->cols; ++col) {
-    plate_matrix->auxiliary_matrix[0][col] = initial_matrix[0][col];
+    plate_matrix->auxiliary_matrix[/*0 * cols + */ col]
+        = initial_matrix[/*0 * cols + */ col];
   }
 
   // Copy last row to auxiliary matrix
   uint64_t last_row = plate_matrix->rows - 1;
   for (uint64_t col = 0; col < plate_matrix->cols; ++col) {
-    plate_matrix->auxiliary_matrix[last_row][col] =
-        initial_matrix[last_row][col];
+    uint64_t index = last_row * plate_matrix-> cols + col;
+    plate_matrix->auxiliary_matrix[index] = initial_matrix[index];
   }
 
   // Copy first column to auxiliary matrix
   for (uint64_t row = 1; row < last_row; ++row) {
-    plate_matrix->auxiliary_matrix[row][0] = initial_matrix[row][0];
+    uint64_t index = row * plate_matrix-> cols /*+ first_col (0)*/;
+    plate_matrix->auxiliary_matrix[index] = initial_matrix[index];
   }
 
   // Copy last column to auxiliary matrix
   uint64_t last_col = plate_matrix->cols - 1;
   for (uint64_t row = 1; row < last_row; ++row) {
-    plate_matrix->auxiliary_matrix[row][last_col] =
-        initial_matrix[row][last_col];
+    uint64_t index = row * plate_matrix-> cols + last_col;
+    plate_matrix->auxiliary_matrix[index] = initial_matrix[index];
   }
 }
 
@@ -68,7 +70,7 @@ void init_auxiliary(plate_matrix_t* plate_matrix) {
 void set_auxiliary(plate_matrix_t* plate_matrix) {
   // Temperatures at current state are in matrix, but we want them to be
   // stored in auxiliary to have space in the main matrix for new temperatures
-  double** current_temperatures = plate_matrix->matrix;
+  double* current_temperatures = plate_matrix->matrix;
 
   // Set matrix pointer to auxiliary's, which holds k-1 state temperatures
   // These will be overwritten
@@ -83,63 +85,29 @@ void set_auxiliary(plate_matrix_t* plate_matrix) {
 
 void update_cell(plate_matrix_t* plate_matrix, uint64_t row,
       uint64_t col, double mult_constant) {
-  double** current_temp_matrix = plate_matrix->auxiliary_matrix;
+  double* current_temp_matrix = plate_matrix->auxiliary_matrix;
+  uint64_t accessed_cell_index = row * plate_matrix->cols + col;
 
   // Compute net energy change using the heat diffusion equation
-  double result = -4 * current_temp_matrix[row][col];
-  result += current_temp_matrix[row - 1][col];  // Top neighbor
-  result += current_temp_matrix[row][col + 1];  // Right neighbor
-  result += current_temp_matrix[row + 1][col];  // Bottom neighbor
-  result += current_temp_matrix[row][col - 1];  // Left neighbor
+  double result = -4 * current_temp_matrix[accessed_cell_index];
+  // Top neighbor
+  result += current_temp_matrix[(row - 1) * plate_matrix->cols + col];
+  result += current_temp_matrix[accessed_cell_index + 1];  // Right neighbor
+  // Bottom neighbor
+  result += current_temp_matrix[(row + 1) * plate_matrix->cols + col];
+  result += current_temp_matrix[accessed_cell_index - 1];  // Left neighbor
 
   // Apply thermal diffusivity, interval duration, and area
   // and add the current temperature
   result *= mult_constant;
-  result += current_temp_matrix[row][col];
+  result += current_temp_matrix[accessed_cell_index];
 
   // Store the new value in the main matrix, with new temperatures
-  plate_matrix->matrix[row][col] = result;
+  plate_matrix->matrix[accessed_cell_index] = result;
 }
-
-
-
-// CODE FROM TEAM_SHOT_PUT
-double** create_double_matrix(const size_t rows, const size_t cols) {
-  double** matrix = (double**)calloc(rows, sizeof(double*));
-  if (matrix) {
-    // Initialize rows
-    for (size_t row = 0; row < rows; ++row) {
-      // Make sure row was allocated successfully with if
-      if ((matrix[row] = (double*)calloc(cols, sizeof(double))) == NULL) {
-          destroy_double_matrix(matrix, rows);
-          return NULL;
-      }
-    }
-  }
-  return matrix;
-}
-
-
 
 void destroy_plate_matrix(plate_matrix_t* plate_matrix) {
-  uint64_t rows = plate_matrix->rows;
-
-  // Calls double matrix destruction subroutines
-  destroy_double_matrix(plate_matrix->matrix, rows);
-  destroy_double_matrix(plate_matrix->auxiliary_matrix, rows);
-
+  free(plate_matrix->matrix);
+  free(plate_matrix->auxiliary_matrix);
   free(plate_matrix);
-}
-
-
-
-// CODE FROM TEAM_SHOT_PUT
-void destroy_double_matrix(double** matrix, const uint64_t rows) {
-  if (matrix) {
-    // Free by row
-    for (size_t row = 0; row < rows; ++row) {
-        free(matrix[row]);
-    }
-    free(matrix);
-  }
 }
