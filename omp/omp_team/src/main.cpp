b22b1276f7b700c194fc4a33af50006c04dcc419
Evan Chen <evan.chen@ucr.ac.cr>
2025-05-13 11:40:52 -0600

    omp/omp_team: partial solution added

--- omp/omp_team/src/main.cpp
+++ omp/omp_team/src/main.cpp
@@ -1,9 +1,32 @@
 // Copyright 2024 ECCI-UCR CC-BY 4.0
 #include <iostream>
+#include <omp.h>
+
+void heavy_task() {
+}
 
 int main(int argc, char* argv[]) {
-  const int thread_count = argc == 2 ? ::atoi(argv[1]) : 1;
+  // If no thead count specified, use omp max amount of threads (core count)
+  const int thread_count = argc == 2 ? ::atoi(argv[1]) :
+      omp_get_max_threads();
 
-  std::cout << "Hello from secondary thread " << 0
-    << " of " << thread_count << std::endl;
+  // Open MP is declarative: we don't have as much control but it's easy
+  // PARALLEL IMPLIES THREAD TEAM CREATION
+  // numm_threads clause sets thread count as amount of threads used
+  #pragma omp parallel num_threads(thread_count)
+  {
+    heavy_task();  // Hypothetical subroutine
+    // Prints this thread_count of times in parallel
+
+    // At processor level, it generates a routine and expands into pthread code
+
+    // Parallel block with critical, indicating that it's a critical region
+    // This is the equivalent of adding a mutex, named stdout to differentiate
+    #pragma omp critical(stdout)
+    std::cout << "Hello from secondary thread " << omp_get_thread_num()
+      << " of " << thread_count << std::endl;
+  }
+  // Implicit join after execution. Main thread continues after this
+  // Thus the following print will always occur after secondary greets
+  std::cout << "Hello from main thread " << std::endl;
 }
