209d88e5ca91f7b1356521cd644caa996070f091
Evan Chen Cheng --CI-0117-03 <C32200@poas.ecci.ucr.ac.cr>
2025-05-14 09:41:50 -0600

    homeworks/serial: updated serial sh file

--- homeworks/serial/tests/serial0.sh
+++ homeworks/serial/tests/serial0.sh
@@ -2,10 +2,10 @@
 
 #SBATCH --overcommit          # Allow more processes than CPUs per node
 #SBATCH -J bin/serial         # Job name
-#SBATCH -o jobSerial0.%j.txt  # Stdout output file (%j expands to jobId)
+#SBATCH -o jobSerial0J20.%j.txt  # Stdout output file (%j expands to jobId)
 #SBATCH -N 1                  # Total number of nodes requested
 #SBATCH -n 1                  # Total number of processes requested
-#SBATCH -t 0:10:00            # Run time (hh:mm:ss) - 10 min
+#SBATCH -t 02:00:00            # Run time (hh:mm:ss) - 1 hr
 #SBATCH -p normal             # Partition to use (default, normal, debug)
 
-perf stat bin/serial jobs/job003b/job003.txt
+perf stat bin/serial jobs/job020/job020.txt
