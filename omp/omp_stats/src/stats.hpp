1efadd841dfc1f0b1160b6c78581a47ca52d2ad0
Evan Chen <evan.chen@ucr.ac.cr>
2025-05-20 12:03:22 -0600

    omp/omp_stats: Missing median

--- omp/omp_stats/src/stats.hpp
+++ omp/omp_stats/src/stats.hpp
@@ -5,10 +5,13 @@
 #include <limits>
 #include <cmath>
 #include <vector>
+#include <omp.h>
 
 template <typename Type>
 Type minimum(const std::vector<Type>& values) {
   Type result = std::numeric_limits<Type>::max();
+  // #pragma omp parallel for schedule(static) reduction(min:result) \
+  //     default(none) shared(values)
   for (size_t index = 0; index < values.size(); ++index) {
     if (values[index] < result) {
       result = values[index];
@@ -20,6 +23,8 @@ Type minimum(const std::vector<Type>& values) {
 template <typename Type>
 Type maximum(const std::vector<Type>& values) {
   Type result = std::numeric_limits<Type>::min();
+  // #pragma omp parallel for schedule(static) reduction(max:result) \
+  //     default(none) shared(values)
   for (size_t index = 0; index < values.size(); ++index) {
     if (values[index] > result) {
       result = values[index];
@@ -31,11 +36,41 @@ Type maximum(const std::vector<Type>& values) {
 template <typename Type>
 Type sum(const std::vector<Type>& values) {
   Type result = Type();
+  // #pragma omp parallel for schedule(static) reduction(+:result) \
+  //     default(none) shared(values)
   for (size_t index = 0; index < values.size(); ++index) {
     result += values[index];
   }
   return result;
 }
+
+
+// template <typename Type>
+// Type sum(const std::vector<Type>& values) {
+//   Type result = Type();
+//   // The values vector and result are shared variables
+//   #pragma omp parallel default(none) shared(values, result)
+//   {
+//     // {} is an initialization
+//     Type mySum = {};
+//     // static map is superior in this problem given:
+//     // 1) Each region takes the same amount of time to complete
+//     // 2) Avoids false sharing if possible
+//     #pragma omp for schedule(static)
+//     for (size_t index = 0; index < values.size(); ++index) {
+//       mySum += values[index];
+//     }
+//     // ALT #pragma omp critical(can_acces_result)
+
+//     // atomic instructions don't remove threads from CPU, like mutex does,
+//     // instead it assures the other cores pause and only one CPU executes
+//     // Only works for simple instructions
+//     #pragma omp atomic
+//     result += mySum;
+//   }
+
+//   return result;
+// }
 
 template <typename Type>
 double average(const std::vector<Type>& values) {
@@ -52,6 +87,8 @@ double std_dev(const std::vector<Type>& values) {
   if (values.size() >= 2) {
     const double avg = average(values);
     double square_sum = 0.0;
+    #pragma omp parallel for schedule(static) reduction(+:square_sum) \
+        default(none) shared(values, avg)
     for (size_t index = 0; index < values.size(); ++index) {
       square_sum += square(values[index] - avg);
     }
