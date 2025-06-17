c378941140e91594a37d1faa361cb661f349a258
Evan Chen <evan.chen@ucr.ac.cr>
2025-06-17 11:48:30 -0600

    send_recv_ord_itm: Solved

--- mpi/send_recv_ord_itm/src/main.cpp
+++ mpi/send_recv_ord_itm/src/main.cpp
@@ -3,14 +3,33 @@
 
 #include "Mpi.hpp"
 
+#include <sstream>
+
+#define FIRST_PROCESS 0
+
 // 70 chars for "Hello from main thread of process <i> of <w> on <hostname>"
+// constexpr indicates the expression does not have address, is constant
 constexpr size_t MAX_MSG_LEN = MPI_MAX_PROCESSOR_NAME + 70;
 
 int main(int argc, char* argv[]) {
   try {
     Mpi mpi(argc, argv);
-    std::cout << "Hello from main thread of process " << mpi.rank()
-        << " of " << mpi.size() << " on " << mpi.getHostname() << std::endl;
+    std::stringstream message;
+    message << "Hello from main thread of process " << mpi.rank()
+        << " of " << mpi.size() << " on " << mpi.getHostname();
+  
+    if (mpi.rank() > FIRST_PROCESS) {
+      // send msg to first process to print
+      mpi.send(message.str(), FIRST_PROCESS);
+    } else {
+      std::cout << message.str() << std::endl;
+      // Iterate through processes (to print in order)
+      for (int source = FIRST_PROCESS + 1; source < mpi.size(); ++source) {
+        std::string buffer;
+        mpi.receive(buffer, MAX_MSG_LEN, source);  // Receive msg
+        std::cout << buffer << std::endl;  // Print process's msg
+      }
+    }
   } catch (const std::exception& error) {
     std::cerr << "error: " << error.what() << std::endl;
   }
