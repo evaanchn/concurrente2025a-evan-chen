3b00a71017a5f2cc5f8171a6c19683f2648134ff
Evan Chen <evan.chen@ucr.ac.cr>
2025-06-17 12:04:46 -0600

    mpi_ping_pong: advanced

--- mpi/mpi_ping_pong/src/main.cpp
+++ mpi/mpi_ping_pong/src/main.cpp
@@ -4,13 +4,18 @@
 
 #include "Mpi.hpp"
 
-void play_ping_pong(Mpi& mpi);
+#define FIRST_PROCESS 0
+void play_ping_pong(Mpi& mpi, const long delay);
 
 int main(int argc, char* argv[]) {
   try {
     Mpi mpi(argc, argv);
-    const long delay = argc >= 2 ? std::stol(argv[1]) : 500;  // milliseconds
-    play_ping_pong(mpi, delay);
+    if (mpi.size() == 2) {
+      const long delay = argc >= 2 ? std::stol(argv[1]) : 500;  // milliseconds
+      play_ping_pong(mpi, delay);
+    } else if (mpi.rank() == 0) {
+      std::cerr << "Only two processes are allowed" << std::endl;
+    }
   } catch (const Mpi::Error& error) {
     std::cerr << "error: " << error.what() << std::endl;
   }
@@ -18,8 +23,18 @@ int main(int argc, char* argv[]) {
 }
 
 void play_ping_pong(Mpi& mpi, const long delay) {
+  long ball = 0;
+  // Process 0 serves first
+  if (mpi.rank() == FIRST_PROCESS) {
+    mpi.send(ball, FIRST_PROCESS + 1);  // Send ball to the other process
+  }
+
   while (true) {
-    std::cout << mpi.rank() << " serves" << std::endl;
+    mpi.receive(ball);  // Wait to receive ball
+    std::cout << mpi.rank() << " serves " << ball << std::endl;  // Report ball
+    ++ball;
     usleep(1000 * delay);
+    int otherPlayer = (mpi.rank() + 1) % 2;  // Calculate the other process
+    mpi.send(ball, otherPlayer);  // Send to the 
   }
 }
